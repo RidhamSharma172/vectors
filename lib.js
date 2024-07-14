0eb36ce6158e7dcbc569563e8bae1f60350e0ae1
Ridham Sharma <118728461+RidhamSharma172@users.noreply.github.com>
2024-07-14 18:49:16 +0530

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,48 @@
+class Position {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getPosition() {
+        return { x: this.x, y: this.y };
+    }
+
+    addVelocity(velocity) {
+        this.x += velocity.x;
+        this.y += velocity.y;
+        return this;
+    }
+}
+
+class Velocity {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    addVelocity(x = 0, y = 0) {
+        if (typeof x == "object") {
+            this.x += x.x;
+            this.y += y.y;
+            return this;
+        }
+        this.x += x;
+        this.y += y;
+        return this;
+    }
+}
+
+function normalise(vector) {
+    const magnitude = Math.hypot(vector.x, vector.y);
+    const newVector = {...vector};
+    newVector.x = newVector.x / magnitude;
+    newVector.y = newVector.y / magnitude;
+    return newVector;
+}
+
+module.exports = {
+    Position: Position,
+    Velocity: Velocity,
+    normalise: normalise
+}
