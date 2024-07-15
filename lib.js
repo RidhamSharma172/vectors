f2e9cb2de8f185c3cda0f7244ac6ee3fe794c27b
Ridham Sharma <118728461+RidhamSharma172@users.noreply.github.com>
2024-07-15 10:14:06 +0530

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,48 +1,112 @@
 class Position {
+    #x;
+    #y;
+    #direction;
+    #magnitude;
+
     constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
+        this.#x = x;
+        this.#y = y;
+        this.#direction = normalise(x, y);
+        this.#magnitude = Math.hypot(x, y);
     }
 
-    getPosition() {
-        return { x: this.x, y: this.y };
+    update(x = 0, y = 0) {
+        this.#x = x;
+        this.#y = y;
+        this.#direction = normalise(x, y);
+        this.#magnitude = Math.hypot(x, y);
+    }
+
+    getVector() {
+        return {
+            x: this.#x,
+            y: this.#y,
+            direction: this.#direction,
+            magnitude: this.#magnitude
+        }
     }
 
     addVelocity(velocity) {
-        this.x += velocity.x;
-        this.y += velocity.y;
+        this.#x += velocity.x;
+        this.#y += velocity.y;
         return this;
     }
 }
 
 class Velocity {
+    #x;
+    #y;
+    #direction;
+    #magnitude;
+
     constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
+        this.#x = x;
+        this.#y = y;
+        this.#direction = normalise(x, y);
+        this.#magnitude = Math.hypot(x, y);
     }
 
-    addVelocity(x = 0, y = 0) {
+    getVector() {
+        return {
+            x: this.#x,
+            y: this.#y,
+            direction: this.#direction,
+            magnitude: this.#magnitude
+        }
+    }
+
+    update(x = 0, y = 0) {
+        this.#x = x;
+        this.#y = y;
+        this.#direction = normalise(x, y);
+        this.#magnitude = Math.hypot(x, y);
+    }
+
+    add(x = 0, y = 0) {
         if (typeof x == "object") {
-            this.x += x.x;
-            this.y += x.y;
+            this.#x += x.x;
+            this.#y += x.y;
             return this;
         }
-        this.x += x;
-        this.y += y;
+        this.#x += x;
+        this.#y += y;
         return this;
     }
 }
 
-function normalise(vector) {
-    const magnitude = Math.hypot(vector.x, vector.y);
-    const newVector = {...vector};
-    newVector.x = newVector.x / magnitude;
-    newVector.y = newVector.y / magnitude;
-    return newVector;
+function normalise(x, y) {
+    const magnitude = Math.hypot(x, y);
+    const newX = x / magnitude;
+    const newY = y / magnitude;
+    return { x: newX, y: newY};
+}
+
+function getAngleBetweenVectors(vector1, vector2) {
+    const _vector1 = getPolarComponents(vector1.x, vector1.y);
+    const _vector2 = getPolarComponents(vector2.x, vector2.y);
+    return Math.abs(_vector1.direction - _vector2.direction);
+}
+
+function getPolarComponents(x, y) {
+    const magnitude = Math.hypot(x, y);
+    const direction = Math.atan2(x, y);
+
+    return {magnitude: magnitude, direction: direction};
+}
+
+function getCartesianComponents(magnitude, direction) {
+    const x = Math.cos(direction) * magnitude;
+    const y = Math.sin(direction) * magnitude;
+
+    return {x: x, y: y}
 }
 
 module.exports = {
     Position: Position,
     Velocity: Velocity,
-    normalise: normalise
+    normalise: normalise,
+    getAngleBetweenVectors: getAngleBetweenVectors,
+    getPolarComponents: getPolarComponents,
+    getCartesianComponents: getCartesianComponents
 }
